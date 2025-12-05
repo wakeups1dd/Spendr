974ac9916ac124fa9b8dfde7f517a4a8eb2a9ea3
wakeups1dd <siddhantgangawane335@gmail.com>
2025-12-05 21:47:13 +0530

    finalized

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -68,7 +68,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
             email,
             password,
             options: {
-                emailRedirectTo: `${window.location.origin}/auth/callback`,
+                emailRedirectTo: `https://spendr-ecru.vercel.app/auth/callback`,
             },
         });
 
